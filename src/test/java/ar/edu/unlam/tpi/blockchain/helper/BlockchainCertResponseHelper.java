6b541abd250989538167bd4547704f33c2cc8c2e
ARUQUIPA FLORES ALAN NAHUEL <aaruquipaflores639@alumno.unlam.edu.ar>
2025-05-29 23:27:48 -0300

    feat: fixing swagger

---
+++ src/test/java/ar/edu/unlam/tpi/blockchain/helper/BlockchainCertResponseHelper.java
@@ -0,0 +1,16 @@
+package ar.edu.unlam.tpi.blockchain.helper;
+
+import ar.edu.unlam.tpi.blockchain.dto.response.BlockchainCertResponseDTO;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class BlockchainCertResponseHelper {
+    
+    public BlockchainCertResponseDTO createBlockchainCertResponseDTO(){
+        return BlockchainCertResponseDTO.builder()
+                .txHash("0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef")
+                .dataHash("0xabcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890")
+                .blockNumber(123456)
+                .build(); 
+    }
+}
